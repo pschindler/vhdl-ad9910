27c2659b8797fb2c46ac7e3112d42c6f30bfdd0f
viellieb <viellieb@ohm>
2008-01-04 21:52:23 +0100

    restructured the serial state machine. changed the clk for the fifo

--- dds_controller.vhd
+++ dds_controller.vhd
@@ -1,6 +1,5 @@
 -- -*- mode: Vhdl -*-
--- Time-stamp: "2008-01-04 17:24:45 c704271"
-
+-- Time-stamp: "04-Jan-2008 21:48:36 viellieb"
 -- file dds_controller.vhd
 -- copyright (c) Philipp Schindler 2008
 -- url http://pulse-sequencer.sf.net
@@ -72,18 +71,18 @@ architecture behaviour of dds_controller is
   signal aux_ser_act    : std_logic;
   signal aux_ser_reset  : std_logic;
 
-  signal aux_ser_ovr           : std_logic_vector(SER_REGWIDTH-1 downto 0);
-  signal aux_ser_state         : std_logic_vector(2 downto 0);
-  signal aux_ser_cur_state         : std_logic_vector(2 downto 0);
-  signal aux_ser_fifo_active   : std_logic;
-  signal aux_ser_data          : std_logic_vector(DATAWIDTH-1 downto 0);
+  signal aux_ser_ovr           :     std_logic_vector(SER_REGWIDTH-1 downto 0);
+  signal aux_ser_state         :     std_logic_vector(2 downto 0);
+  signal aux_ser_cur_state     :     std_logic_vector(2 downto 0);
+  signal aux_ser_fifo_active   :     std_logic;
+  signal aux_ser_data          :     std_logic_vector(DATAWIDTH-1 downto 0);
   -- aux_signals for FIFO control
-  signal aux_rd_fifo           : std_logic;
-  signal aux_wr_fifo           : std_logic;
-  signal aux_fifo_empty        : std_logic;
-  signal aux_fifo_out          : std_logic_vector(DATAWIDTH-1 downto 0);
+  signal aux_rd_fifo           :     std_logic;
+  signal aux_wr_fifo           :     std_logic;
+  signal aux_fifo_empty        :     std_logic;
+  signal aux_fifo_out          :     std_logic_vector(DATAWIDTH-1 downto 0);
   -- aux signals for the dds ioupdate
-  signal aux_profile_state     : std_logic_vector(2 downto 0);
+  signal aux_profile_state     :     std_logic_vector(2 downto 0);
   --- The opcode from the bus
   alias opcode_data is bus_in(BUSWIDTH-1 downto BUSWIDTH-OPCODE_WIDTH);
   -- The address from the bus
@@ -91,30 +90,32 @@ architecture behaviour of dds_controller is
   -- The handshake data from the bus
   alias data_avail is bus_in(BUSWIDTH-OPCODE_WIDTH-ADDRESWIDTH-1);
   --- The async decoded signals
-  signal decoded_reset         : boolean;
-  signal decoded_fifo_wr       : boolean;
-  signal decoded_dds_addr      : boolean;
-  signal decoded_dds_profile   : boolean;
-  signal decoded_dac_amplitude : boolean;
-  signal decoded_dds_phase     : boolean;
-  signal address_bit           : boolean;
-  signal avail_bool            : boolean;
-
+  signal decoded_reset         :     boolean;
+  signal decoded_fifo_wr       :     boolean;
+  signal decoded_dds_addr      :     boolean;
+  signal decoded_dds_profile   :     boolean;
+  signal decoded_dac_amplitude :     boolean;
+  signal decoded_dds_phase     :     boolean;
+  signal address_bit           :     boolean;
+  signal avail_bool            :     boolean;
+  signal aux_clk_state_cur     :     unsigned(1 downto 0);
+  signal aux_clk_state         :     unsigned(1 downto 0);
+  signal aux_clk               :     std_logic_vector;
 -------------------------------------------------------------------------------
 -- Serial bus controller
 -------------------------------------------------------------------------------
   component dds_serial_bus
     port (
-      reset       : in  std_logic;
-      wb_clk      : in  std_logic;
-      data        : in  std_logic_vector (DATAWIDTH-1 downto 0);
+      reset                    : in  std_logic;
+      wb_clk                   : in  std_logic;
+      data                     : in  std_logic_vector (DATAWIDTH-1 downto 0);
 -- data2 : out std_logic_vector (DATAWIDTH-1 downto 0);
-      load_reg    : in  std_logic;
-      done_out    : out std_logic;
-      sclk_out    : out std_logic;
-      active_flag      : in  std_logic;
-      counter_ovr : in  std_logic_vector(SER_REGWIDTH-1 downto 0);
-      sdo_out     : out std_logic
+      load_reg                 : in  std_logic;
+      done_out                 : out std_logic;
+      sclk_out                 : out std_logic;
+      active_flag              : in  std_logic;
+      counter_ovr              : in  std_logic_vector(SER_REGWIDTH-1 downto 0);
+      sdo_out                  : out std_logic
       );
 
   end component;
@@ -166,19 +167,19 @@ begin
   decoded_dds_profile   <= (opcode_data = dds_profile_opcode) and address_bit and avail_bool;
   decoded_dac_amplitude <= (opcode_data = dac_amplitude_opcode) and address_bit and avail_bool;
   decoded_dds_phase     <= (opcode_data = dds_phase_ctl_opcode) and address_bit and avail_bool;
-  aux_ser_enable        <= '1'  when decoded_dds_addr else '0';
-  avail_bool            <= true when data_avail = '1' else false;
-  address_bit           <= (address_data = address_in) when USE_ADDRESSING else true;
+  aux_ser_enable        <= '1'                         when decoded_dds_addr else '0';
+  avail_bool            <= true                        when data_avail = '1' else false;
+  address_bit           <= (address_data = address_in) when USE_ADDRESSING   else true;
 -------------------------------------------------------------------------------
 -- Asynchronious reset
 -------------------------------------------------------------------------------
-aux_ser_reset           <= '1'  when decoded_reset    else '0';
-  aux_reset             <= '1'  when decoded_reset    else '0';
+  aux_ser_reset         <= '1'                         when decoded_reset    else '0';
+  aux_reset             <= '1'                         when decoded_reset    else '0';
 
 -------------------------------------------------------------------------------
 -- Asynchronious control the parallel data out
 -------------------------------------------------------------------------------
---  parallel_data <= bus_in(DATAWIDTH-1 downto 0);
+-- parallel_data <= bus_in(DATAWIDTH-1 downto 0);
 -- parallel_data <= phase_register_out when decoded_dds_phase else bus_in(DATAWIDTH-1 downto 0);
 
 -------------------------------------------------------------------------------
@@ -192,7 +193,7 @@ aux_ser_reset           <= '1'  when decoded_reset    else '0';
       sdo_out     => sdo_pin(0),
       sclk_out    => sclk_pin,
       load_reg    => aux_ser_load,
-      active_flag      => aux_ser_act,
+      active_flag => aux_ser_act,
       counter_ovr => aux_ser_ovr,
       done_out    => aux_ser_done
       );
@@ -202,7 +203,7 @@ aux_ser_reset           <= '1'  when decoded_reset    else '0';
   fifo_mf_inst   : fifo_mf
     port map (
       aclr        => aux_reset,
-      clock       => clk0,
+      clock       => aux_clk,
       data        => bus_in(DATAWIDTH-1 downto 0),
       rdreq       => aux_rd_fifo,
       wrreq       => aux_wr_fifo,
@@ -219,106 +220,101 @@ aux_ser_reset           <= '1'  when decoded_reset    else '0';
 -- First the adrress byte is taken from the BUS and then the FIFO is emptied.
   state_control : process(clk0)
   begin
-    if rising_edge(clk0) and aux_reset='0' and aux_ser_enable = '1'  then
+    if rising_edge(clk0) and aux_reset = '0' and aux_ser_enable = '1' then
       aux_ser_cur_state <= aux_ser_state;
     end if;
-    if aux_ser_enable='0' then
+    if aux_ser_enable = '0' then
       aux_ser_cur_state <= B"000";
     end if;
-    if aux_reset='1' then
+    if aux_reset = '1' then
       aux_ser_cur_state <= B"000";
     end if;
   end process;
 
   serial_control : process(aux_ser_cur_state)
   begin
-    debug_out(1) <=  aux_rd_fifo;
-    debug_out(0) <= aux_ser_done;
-    debug_out(3) <= aux_ser_act;
---    parallel_data <= aux_fifo_out;
---    debug_out(2 downto 0) <= aux_ser_state;
---    if rising_edge(clk0) then
---      if aux_ser_enable = '1' then
-        case aux_ser_cur_state is
-          -- send an ioreset before ???
-          when B"000" => aux_rd_fifo     <= '0';  -- Load the address byte
-                         aux_ser_act     <= '0';
-                         aux_ser_load    <= '1';
-                         aux_ser_ovr     <= BYTE_OVERRUN;
-                         aux_ser_state   <= B"001";
-                         aux_ser_data    <= bus_in(DATAWIDTH -1 downto 0);
-                         ioreset_pin     <= '1';
-          when B"001" => aux_rd_fifo     <= '0';  -- Wait until the address byte is sent
-                         aux_ser_act     <= '1';
-                         aux_ser_load    <= '0';
-                         ioreset_pin     <= '0';
-                         aux_ser_ovr     <= BYTE_OVERRUN;
-                         aux_ser_data    <= bus_in(DATAWIDTH -1 downto 0);
-                         if aux_ser_done = '1' then
-                           aux_ser_state <= B"010";
-                         else
-                           aux_ser_state <= B"001";
-                         end if;
-          when B"010" => aux_rd_fifo     <= '1';  -- Load the FIFO word
-                         aux_ser_act     <= '0';
-                         aux_ser_load    <= '0';
-                         aux_ser_ovr     <= BYTE_OVERRUN;
-                         ioreset_pin     <= '0';
-                         aux_ser_state   <= B"011";
-                         aux_ser_data    <= aux_fifo_out;
+    debug_out(1)                   <= aux_rd_fifo;
+    debug_out(0)                   <= aux_ser_done;
+    debug_out(3)                   <= aux_ser_act;
+-- parallel_data <= aux_fifo_out;
+-- debug_out(2 downto 0) <= aux_ser_state;
+    case aux_ser_cur_state is
+      -- send an ioreset before ???
+      when B"000" => aux_rd_fifo   <= '0';  -- Load the address byte
+                     aux_ser_act   <= '0';
+                     aux_ser_load  <= '0';
+                     aux_ser_ovr   <= BYTE_OVERRUN;
+                     aux_ser_state <= B"001";
+                     aux_ser_data  <= bus_in(DATAWIDTH -1 downto 0);
+                     ioreset_pin   <= '0';
 
-          when B"011" => aux_ser_state <= B"100";
-                         aux_ser_ovr     <= BYTE_OVERRUN;
-                         aux_ser_load    <= '0';
-                         aux_ser_act   <= '0';
-                         aux_rd_fifo   <= '0';
-                         aux_ser_data  <= aux_fifo_out;
-                         ioreset_pin   <= '0';
-          when B"100" => aux_ser_state <= B"101";  -- Wait until the FIFO has set the data
-                         aux_ser_data  <= aux_fifo_out;
-                         aux_ser_ovr     <= FULL_OVERRUN;
-                         aux_ser_load    <= '0';
-                         ioreset_pin   <= '0';
-                         aux_ser_act   <= '0';
-                         aux_rd_fifo   <= '0';
-          when B"101" => aux_ser_state <= B"110";  -- Wait until the FIFO has set the data
-                         aux_ser_data  <= aux_fifo_out;
-                         aux_ser_ovr     <= FULL_OVERRUN;
-                         aux_ser_load    <= '1';
-                         ioreset_pin   <= '0';
-                         aux_ser_act   <= '0';
-                         aux_rd_fifo   <= '0';
-                         -- Wait until the FIFO word is sent / loop until FIFO is empty
+      when B"001" => aux_rd_fifo     <= '0';  -- Load the address byte
+                     aux_ser_act     <= '0';
+                     aux_ser_load    <= '1';
+                     aux_ser_ovr     <= BYTE_OVERRUN;
+                     aux_ser_state   <= B"010";
+                     aux_ser_data    <= bus_in(DATAWIDTH -1 downto 0);
+                     ioreset_pin     <= '1';
+      when B"010" => aux_rd_fifo     <= '0';  -- Wait until the address byte is sent
+                     aux_ser_act     <= '1';
+                     aux_ser_load    <= '0';
+                     ioreset_pin     <= '0';
+                     aux_ser_ovr     <= BYTE_OVERRUN;
+                     aux_ser_data    <= bus_in(DATAWIDTH -1 downto 0);
+                     if aux_ser_done = '1' then
+                       aux_ser_state <= B"011";
+                     else
+                       aux_ser_state <= B"010";
+                     end if;
+      when B"011" => aux_rd_fifo     <= '1';  -- Load the FIFO word
+                     aux_ser_act     <= '0';
+                     aux_ser_load    <= '0';
+                     aux_ser_ovr     <= BYTE_OVERRUN;
+                     ioreset_pin     <= '0';
+                     aux_ser_state   <= B"100";
+                     aux_ser_data    <= aux_fifo_out;
 
-          when B"110" => aux_rd_fifo       <= '0';
-                         aux_ser_act       <= '1';
-                         aux_ser_ovr     <= FULL_OVERRUN;
-                         aux_ser_data      <= aux_fifo_out;
-                         aux_ser_load      <= '0';
-                         ioreset_pin       <= '0';
-                         if aux_ser_done = '1' then
-                           if aux_fifo_empty = '1' then
-                             aux_ser_state <= B"111";
-                           else
-                             aux_ser_state <= B"010";
-                           end if;
-                         else
-                           aux_ser_state <= B"110";
-                         end if;
-          when B"111" => aux_rd_fifo       <= '0';
-                         aux_ser_state     <= B"000";  -- Wait until the opcode changes
-                         ioreset_pin       <= '0';
-                         aux_ser_act       <= '0';
-          when others => aux_ser_state     <= B"000";  -- generate initial state
-                         aux_ser_load      <= '0';
-                         aux_rd_fifo       <= '0';
-                         ioreset_pin       <= '0';
-                         aux_ser_act       <= '0';
-        end case;
---      else
---        aux_ser_state                      <= B"000";
---      end if;
---    end if;
+      when B"100" => aux_ser_state <= B"101";  -- Wait until the FIFO has set the data
+                     aux_ser_data  <= aux_fifo_out;
+                     aux_ser_ovr   <= FULL_OVERRUN;
+                     aux_ser_load  <= '0';
+                     ioreset_pin   <= '0';
+                     aux_ser_act   <= '0';
+                     aux_rd_fifo   <= '1';
+      when B"101" => aux_ser_state <= B"110";  -- Wait until the FIFO has set the data
+                     aux_ser_data  <= aux_fifo_out;
+                     aux_ser_ovr   <= FULL_OVERRUN;
+                     aux_ser_load  <= '1';
+                     ioreset_pin   <= '0';
+                     aux_ser_act   <= '0';
+                     aux_rd_fifo   <= '0';
+                     -- Wait until the FIFO word is sent / loop until FIFO is empty
+
+      when B"110" => aux_rd_fifo       <= '0';
+                     aux_ser_act       <= '1';
+                     aux_ser_ovr       <= FULL_OVERRUN;
+                     aux_ser_data      <= aux_fifo_out;
+                     aux_ser_load      <= '0';
+                     ioreset_pin       <= '0';
+                     if aux_ser_done = '1' then
+                       if aux_fifo_empty = '1' then
+                         aux_ser_state <= B"111";
+                       else
+                         aux_ser_state <= B"010";
+                       end if;
+                     else
+                       aux_ser_state   <= B"110";
+                     end if;
+      when B"111" => aux_rd_fifo       <= '0';
+                     aux_ser_state     <= B"000";  -- Wait until the opcode changes
+                     ioreset_pin       <= '0';
+                     aux_ser_act       <= '0';
+      when others => aux_ser_state     <= B"000";  -- generate initial state
+                     aux_ser_load      <= '0';
+                     aux_rd_fifo       <= '0';
+                     ioreset_pin       <= '0';
+                     aux_ser_act       <= '0';
+    end case;
   end process;
 
 -------------------------------------------------------------------------------
@@ -355,19 +351,10 @@ aux_ser_reset           <= '1'  when decoded_reset    else '0';
   control_profile : process(clk0)
   begin
     if rising_edge(clk0) then
-      if not decoded_reset then
+      if aux_reset='0' then
         if decoded_dds_profile then
           aux_profile_state <= bus_in(2 downto 0);
           ioup_pin          <= '1';
--- discard another beautiuful state machine due to the need for speed
--- profile_pin<=bus_in(2 downto 0);
--- case aux_profile_state is
--- when '0' => ioup_pin<='1';
--- aux_profile_state<='1';
--- when '1' => ioup_pin<='0';
--- aux_profile_state<='0';
--- when others => aux_profile_state<='0';
--- end case;
         else
           ioup_pin          <= '0';
           aux_profile_state <= aux_profile_state;
@@ -380,5 +367,27 @@ aux_ser_reset           <= '1'  when decoded_reset    else '0';
     end if;
   end process;
 
+-------------------------------------------------------------------------------
+-- Clock divider for the FIFO
+-------------------------------------------------------------------------------
+  clk_divider : process(clk0)
+  begin
 
+    if rising_edge(clk0) then
+      aux_clk_state_cur <= aux_clk_state;
+    end if;
+
+    if aux_reset = '1' then
+      aux_clk_state_cur <= 0;
+    end if;
+
+    case aux_clk_state_cur is
+      when 0      => aux_clk       <= '0';
+                     aux_clk_state <= 1;
+      when 1      => aux_clk       <= '1';
+                     aux_clk_state <= 0;
+      when others => aux_clk_state <= 0;
+
+    end case;
+  end process;
 end behaviour;
--- dds_serial_bus.vhd
+++ dds_serial_bus.vhd
@@ -1,9 +1,9 @@
 -- -*- mode: Vhdl -*-
--- Time-stamp: "2008-01-04 17:31:14 c704271"
+-- Time-stamp: "04-Jan-2008 21:48:09 viellieb"
 
---  file       dds_serial_bus.vhd
---  copyright  (c) Philipp Schindler 2008
---  url        http://pulse-sequencer.sf.net
+-- file dds_serial_bus.vhd
+-- copyright (c) Philipp Schindler 2008
+-- url http://pulse-sequencer.sf.net
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -17,12 +17,12 @@ entity dds_serial_bus is
   port (
     wb_clk      : in  std_logic;
     data        : in  std_logic_vector (DATAWIDTH-1 downto 0);
-    reset       : in std_logic;
+    reset       : in  std_logic;
 -- data2 : out std_logic_vector (DATAWIDTH-1 downto 0);
     load_reg    : in  std_logic;
     done_out    : out std_logic;
     sclk_out    : out std_logic;
-    active_flag      : in  std_logic;
+    active_flag : in  std_logic;
     counter_ovr : in  std_logic_vector(SER_REGWIDTH-1 downto 0);
     sdo_out     : out std_logic
     );
@@ -31,74 +31,46 @@ end dds_serial_bus;
 architecture behaviour of dds_serial_bus is
 
   signal aux_reg      : std_logic_vector(DATAWIDTH-1 downto 0);
-  signal aux_reg_cur      : std_logic_vector(DATAWIDTH-1 downto 0);
   signal aux_counter  : std_logic_vector(SER_REGWIDTH-1 downto 0);
   signal aux_finished : boolean;
---  signal load_reg     : std_logic;
-  signal clk_counter  : std_logic_vector(1 downto 0);
-  signal clk_counter_cur  : std_logic_vector(1 downto 0);
+-- signal load_reg : std_logic;
+
+-------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 -- Behaviour starts here
 -------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
 begin
 
-  aux_finished <= (aux_counter = counter_ovr);  --whats bigger equal in vhdl??
---  sclk_out     <= '0' when (aux_finished or load_reg = '1' or active_flag = '0') else wb_clk;
-  done_out     <= '1' when aux_finished else '0';
-  sdo_out     <= aux_reg(DATAWIDTH-1) when active_flag='1' else '0';
-  sclk_out <=  '1' when wb_clk='0' and((not aux_finished) and active_flag='1') else '0';
+  aux_finished      <= (aux_counter = counter_ovr);  --whats bigger equal in vhdl??
+  done_out          <= '1'                  when aux_finished                                               else '0';
+  sdo_out           <= aux_reg(DATAWIDTH-1) when active_flag = '1'                                          else '0';
+  sclk_out          <= '1'                  when wb_clk = '0' and((not aux_finished) and active_flag = '1') else '0';
+  -- The sclk out is inversed with respect to the wb_clk to ensure proper timing
+
 -------------------------------------------------------------------------------
--- Serial write process now with state machine for the serial output
--- Untested !!!
+-- serial write process
 -------------------------------------------------------------------------------
   write_serial_process : process(wb_clk, load_reg)
   begin
     if rising_edge(wb_clk) then
-      if reset='0' and active_flag='1' and not aux_finished then
+      if reset = '0' and active_flag = '1' and not aux_finished then
         aux_reg     <= aux_reg(DATAWIDTH-2 downto 0) & '0';
         aux_counter <= std_logic_vector(unsigned(aux_counter) + 1);
       end if;
     end if;
---    if rising_edge(wb_clk) then
---      if (load_reg = '0') and (not aux_finished) and (reset='0') and active_flag='1'  then
---        case clk_counter_cur is
---          when B"00" =>
---            sdo_out     <= aux_reg_cur(DATAWIDTH-1);
---            sclk_out    <= '1';
---            clk_counter <= B"01";
---          when B"01" =>
---            sdo_out     <= aux_reg_cur(DATAWIDTH-1);
---            sclk_out    <= '0';
---            clk_counter <= B"00";
---            aux_counter <= std_logic_vector(unsigned(aux_counter) + 1);
---            aux_reg     <= aux_reg_cur(DATAWIDTH-2 downto 0) & '0';
---          when others =>
---            sdo_out     <= aux_reg_cur(DATAWIDTH-1);
---            clk_counter <= B"00";
---            sdo_out     <= '0';
---            sclk_out    <= '0';
---        end case;
---      else
---        sclk_out <= '0';
---        sdo_out  <= '0';
---        clk_counter <= B"00";
---      end if;
 
---    end if;
     if (load_reg = '1') then
       clk_counter_cur <= B"00";
-      aux_reg     <= data(DATAWIDTH-1 downto 0);
-      aux_counter <= AUX_RESET;
+      aux_reg         <= data(DATAWIDTH-1 downto 0);
+      aux_counter     <= AUX_RESET;
     end if;
 
     if reset = '1' then
       clk_counter_cur <= B"00";
---      aux_reg     <= X"0000";
-      aux_counter <= AUX_RESET;
+-- aux_reg <= X"0000";                  --don't know why this is a problem => .
+      aux_counter     <= AUX_RESET;
     end if;
   end process;
-
-
 
 end behaviour;
