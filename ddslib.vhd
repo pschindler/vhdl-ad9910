fb2e6fd653f81e0f0275aa6f599d8f8e58935147
c704271 <c704271@medus-c704.uibk.ac.at>
2008-02-13 20:23:37 +0100

    minor changes in the wrapper

--- ddslib.vhd
+++ ddslib.vhd
@@ -1,5 +1,5 @@
 -- -*- mode: Vhdl -*-
--- Time-stamp: "2008-01-02 13:20:33 c704271"
+-- Time-stamp: "2008-02-13 15:39:15 c704271"
 
 --  file       ddslib.vhd
 --  copyright  (c) Philipp Schindler 2008
@@ -16,14 +16,14 @@ package instructions is
 
   subtype OPCODE_TYPE is std_logic_vector(OPCODE_WIDTH-1 downto 0);
 
-  constant nop_opcode           : OPCODE_TYPE := B"00000";
+  constant nop_opcode           : OPCODE_TYPE := B"00000";  -- nop 0x0
   ---- DDS serial write
-  constant dds_addr_opcode      : OPCODE_TYPE := B"00001";
-  constant fifo_wr_opcode       : OPCODE_TYPE := B"00010";
+  constant dds_addr_opcode      : OPCODE_TYPE := B"00001";  -- dds_addr 0x1
+  constant fifo_wr_opcode       : OPCODE_TYPE := B"00010";  -- fifo_wr  0x2
   ---- DDS Profile select
-  constant dds_profile_opcode   : OPCODE_TYPE := B"00011";
+  constant dds_profile_opcode   : OPCODE_TYPE := B"00011";  -- dds_prof 0x3
   ---- DAC write
-  constant dac_amplitude_opcode : OPCODE_TYPE := B"00111";
+  constant dac_amplitude_opcode : OPCODE_TYPE := B"00111";  -- dac_wr   0x7
   ---- DDS phase coherent switching
   constant dds_phase_ctl_opcode : OPCODE_TYPE := B"01000";
   ---- DDS phase on parallel port
--- wrapper/wrapper.vhd
+++ wrapper/wrapper.vhd
@@ -2,50 +2,52 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-ENTITY wrapper IS
+entity wrapper is
 
-	PORT
-	(
-		bus_in  : std_logic_vector(31 downto 0);
-		clk0	: in std_logic;
-		debug_in : in std_logic_vector(3 downto 0);
-		cs_pin : out std_logic;
-		ioreset_pin : out std_logic;
-		ioup_pin : out std_logic;
-		sclk_pin : out std_logic;
-		sdio_pin : out std_logic;
-		sdo : in std_logic;
-		dac_clk : out std_logic;
-		dac_out: out std_logic_vector(13 downto 0)
-	--	sdo_debug : out std_logic
-		
-	);
+  port
+    (
+      bus_in        :     std_logic_vector(31 downto 0);
+      clk0          : in  std_logic;
+      debug_in      : in  std_logic_vector(3 downto 0);
+      cs_pin        : out std_logic;
+      ioreset_pin   : out std_logic;
+      ioup_pin      : out std_logic;
+      sclk_pin      : out std_logic;
+      sdio_pin      : out std_logic;
+      sdo           : in  std_logic;
+      dac_clk       : out std_logic;
+      dac_out       : out std_logic_vector(15 downto 0);
+      parallel_dest : out std_logic_vector(1 downto 0);
+      tx_en_out     : out std_logic
+      --  sdo_debug : out std_logic
+
+      );
 
 
-END wrapper;
+end wrapper;
 
 
-ARCHITECTURE a OF wrapper IS
+architecture a of wrapper is
 
 --component analyzer
---	port (
---		acq_clk : std_logic;
---		acq_data_in : std_logic_vector(30 downto 0);
---		acq_trigger_in: std_logic
---		);
+--  port (
+--    acq_clk : std_logic;
+--    acq_data_in : std_logic_vector(30 downto 0);
+--    acq_trigger_in: std_logic
+--    );
 --end component;
 
-BEGIN
+begin
 
 
 --analyzer_inst : analyzer PORT MAP (
---		acq_clk	 => clk0,
---		acq_data_in	 => bus_in(31 downto 1),
---		acq_trigger_in	 => bus_in(0)
---	);
+--    acq_clk  => clk0,
+--    acq_data_in  => bus_in(31 downto 1),
+--    acq_trigger_in   => bus_in(0)
+--  );
 
 --sdo_debug<=sdo;
-ioreset_pin<='0';
+  ioreset_pin <= '0';
 
 --cs_pin <= debug_in(1);
 --sclk_pin<=debug_in(2);
@@ -54,15 +56,18 @@ ioreset_pin<='0';
 
 
 --dac_clk<=clk0;
-dac_clk<=bus_in(24);
-dac_out(13 downto 6)<=bus_in(23 downto 16);
-dac_out(5 downto 0)<=B"111111";
+  parallel_dest        <= B"01";
+  tx_en_out            <= bus_in(21);
+  dac_clk              <= bus_in(20);
+  dac_out(15 downto 8) <= bus_in(29 downto 22);
+  dac_out(7 downto 0)  <= B"11111111";
 
-sclk_pin<=bus_in(16);
-sdio_pin<=bus_in(17);
-cs_pin<=bus_in(18);
-ioup_pin<=bus_in(19);
+  sclk_pin <= bus_in(16);
+  sdio_pin <= bus_in(17);
+  cs_pin   <= bus_in(18);
+  ioup_pin <= bus_in(19);
+
 
 --dac_out<=B"11111111111111";
 --dac_out<=B"00000000000000";
-END a;
+end a;
